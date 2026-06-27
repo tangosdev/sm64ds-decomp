e148fe97602a606a836c5a1a2999550cc9dda749
Moundistz <111893202+Moundistz@users.noreply.github.com>
2026-06-27 19:13:27 +0200

    Fix: preserve newlines in func_ov063_021163d0

--- src/func_ov063_021163d0.cpp
+++ src/func_ov063_021163d0.cpp
@@ -1,1 +1,10 @@
-//cpp extern "C" void *_ZN5Actor10FindWithIDEj(unsigned int id);  extern "C" unsigned char func_ov063_021163d0(char *c) {     unsigned int id = *(unsigned int *)(c + 0x498);     if (id == 0) return 0;     char *actor = (char *)_ZN5Actor10FindWithIDEj(id);     if (actor == 0) return 0;     return *(unsigned char *)(actor + 0x153); }
+//cpp
+extern "C" void *_ZN5Actor10FindWithIDEj(unsigned int id);
+
+extern "C" unsigned char func_ov063_021163d0(char *c) {
+    unsigned int id = *(unsigned int *)(c + 0x498);
+    if (id == 0) return 0;
+    char *actor = (char *)_ZN5Actor10FindWithIDEj(id);
+    if (actor == 0) return 0;
+    return *(unsigned char *)(actor + 0x153);
+}
