54d099bff0896195664487dff8967bf62c12efb1
Moundistz <111893202+Moundistz@users.noreply.github.com>
2026-06-27 19:13:10 +0200

    Fix: preserve newlines in func_ov022_02112560

--- src/func_ov022_02112560.c
+++ src/func_ov022_02112560.c
@@ -1,1 +1,8 @@
-extern void func_020393a4(int *p, int v); extern int func_ov080_0212714c(char *c, int *d); extern int data_ov022_02112c98;  int func_ov022_02112560(char *c) {     func_020393a4((int *)(c + 0x124), 0x500000);     return func_ov080_0212714c(c, &data_ov022_02112c98) & 0xff; }
+extern void func_020393a4(int *p, int v);
+extern int func_ov080_0212714c(char *c, int *d);
+extern int data_ov022_02112c98;
+
+int func_ov022_02112560(char *c) {
+    func_020393a4((int *)(c + 0x124), 0x500000);
+    return func_ov080_0212714c(c, &data_ov022_02112c98) & 0xff;
+}
