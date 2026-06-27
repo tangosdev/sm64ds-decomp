62a6b7b3417167ed54c75c93d110bc8e5d2128c4
Moundistz <111893202+Moundistz@users.noreply.github.com>
2026-06-27 02:03:08 +0200

    Match func_ov034_02112330

---
+++ src/func_ov034_02112330.c
@@ -0,0 +1,1 @@
+extern void* _ZN5Actor13ClosestPlayerEv(void);  struct ActorObj {     char pad[0x8c8];     void* closestPlayer; };  void func_ov034_02112330(char* c) {     void* player = _ZN5Actor13ClosestPlayerEv();     ((struct ActorObj*)c)->closestPlayer = player; }
