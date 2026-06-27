cf5870e65110a2643fcf2da1ae68a2184cfcce0b
Moundistz <111893202+Moundistz@users.noreply.github.com>
2026-06-27 19:13:07 +0200

    Fix: preserve newlines in func_ov020_02112768

--- src/func_ov020_02112768.c
+++ src/func_ov020_02112768.c
@@ -1,1 +1,12 @@
-extern void _ZN5Model8LoadFileER13SharedFilePtr(void *); extern void LoadBlueCoinModel(void *); extern int G0[]; extern int G1[]; int func_ov020_02112768(char *c) {     *(short *)(c + 0xd4) = 0;     _ZN5Model8LoadFileER13SharedFilePtr(G0);     _ZN5Model8LoadFileER13SharedFilePtr(G1);     LoadBlueCoinModel(c);     return 1; }
+extern void _ZN5Model8LoadFileER13SharedFilePtr(void *);
+extern void LoadBlueCoinModel(void *);
+extern int G0[];
+extern int G1[];
+int func_ov020_02112768(char *c)
+{
+    *(short *)(c + 0xd4) = 0;
+    _ZN5Model8LoadFileER13SharedFilePtr(G0);
+    _ZN5Model8LoadFileER13SharedFilePtr(G1);
+    LoadBlueCoinModel(c);
+    return 1;
+}
