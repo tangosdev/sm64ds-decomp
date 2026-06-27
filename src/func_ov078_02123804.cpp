b74fee016d7eab5ba980b41b4e0b2cba3d123a69
Moundistz <111893202+Moundistz@users.noreply.github.com>
2026-06-27 19:13:31 +0200

    Fix: preserve newlines in func_ov078_02123804

--- src/func_ov078_02123804.cpp
+++ src/func_ov078_02123804.cpp
@@ -1,1 +1,13 @@
-//cpp extern "C" void *_ZN5Actor10FindWithIDEj(unsigned int id); extern "C" int func_ov078_02125c48(char *c, void *p); extern int data_ov078_02126ffc;  extern "C" int func_ov078_02123804(char *c) {     unsigned int id = *(unsigned int *)(c + 0x3a0);     if (id == 0) return 0;     if (_ZN5Actor10FindWithIDEj(id) == 0) return 0;     if ((*(unsigned int *)(c + 0x39c) & 0x4000) == 0) return 0;     func_ov078_02125c48(c, &data_ov078_02126ffc);     return 1; }
+//cpp
+extern "C" void *_ZN5Actor10FindWithIDEj(unsigned int id);
+extern "C" int func_ov078_02125c48(char *c, void *p);
+extern int data_ov078_02126ffc;
+
+extern "C" int func_ov078_02123804(char *c) {
+    unsigned int id = *(unsigned int *)(c + 0x3a0);
+    if (id == 0) return 0;
+    if (_ZN5Actor10FindWithIDEj(id) == 0) return 0;
+    if ((*(unsigned int *)(c + 0x39c) & 0x4000) == 0) return 0;
+    func_ov078_02125c48(c, &data_ov078_02126ffc);
+    return 1;
+}
