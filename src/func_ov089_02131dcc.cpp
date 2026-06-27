e0974889b2cebf29d84b28fa525a29901abe8706
Moundistz <111893202+Moundistz@users.noreply.github.com>
2026-06-27 19:13:37 +0200

    Fix: preserve newlines in func_ov089_02131dcc

--- src/func_ov089_02131dcc.cpp
+++ src/func_ov089_02131dcc.cpp
@@ -1,1 +1,18 @@
-//cpp class Event { public: static void SetBit(unsigned int); };  class ActorBase { public: void MarkForDestruction(); };  extern "C" void func_ov002_020c3dbc(int);  extern "C" void func_ov089_02131dcc(ActorBase *r4, int r1) { func_ov002_020c3dbc(r1); Event::SetBit(0x1d); r4->MarkForDestruction(); }
+//cpp
+class Event {
+public:
+static void SetBit(unsigned int);
+};
+
+class ActorBase {
+public:
+void MarkForDestruction();
+};
+
+extern "C" void func_ov002_020c3dbc(int);
+
+extern "C" void func_ov089_02131dcc(ActorBase *r4, int r1) {
+func_ov002_020c3dbc(r1);
+Event::SetBit(0x1d);
+r4->MarkForDestruction();
+}
