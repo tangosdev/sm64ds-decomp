85658f9fdd25bffd3366381ddc41e4b425a96930
Moundistz <111893202+Moundistz@users.noreply.github.com>
2026-06-27 19:13:16 +0200

    Fix: preserve newlines in func_ov034_02112330

--- src/func_ov034_02112330.c
+++ src/func_ov034_02112330.c
@@ -1,1 +1,11 @@
-extern void* _ZN5Actor13ClosestPlayerEv(void);  struct ActorObj {     char pad[0x8c8];     void* closestPlayer; };  void func_ov034_02112330(char* c) {     void* player = _ZN5Actor13ClosestPlayerEv();     ((struct ActorObj*)c)->closestPlayer = player; }
+extern void* _ZN5Actor13ClosestPlayerEv(void);
+
+struct ActorObj {
+    char pad[0x8c8];
+    void* closestPlayer;
+};
+
+void func_ov034_02112330(char* c) {
+    void* player = _ZN5Actor13ClosestPlayerEv();
+    ((struct ActorObj*)c)->closestPlayer = player;
+}
