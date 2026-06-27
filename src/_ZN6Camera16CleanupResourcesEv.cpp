424b15c39a1b39ddaadd8fec0591a7effcf44506
Moundistz <111893202+Moundistz@users.noreply.github.com>
2026-06-27 02:02:12 +0200

    Match _ZN6Camera16CleanupResourcesEv

---
+++ src/_ZN6Camera16CleanupResourcesEv.cpp
@@ -0,0 +1,1 @@
+//cpp extern "C" {     extern void _ZN6Memory16operator_delete2EPv(void *);     int _ZN6Camera16CleanupResourcesEv(void *thiz) {         int p = *(int *)((char *)thiz + 0x148);         if (p != 0 && p != 0) {             _ZN6Memory16operator_delete2EPv((void *)p);         }         return 1;     } }
