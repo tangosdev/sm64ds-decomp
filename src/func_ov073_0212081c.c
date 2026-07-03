96f8c7c7af9373e675e3f738a949795f63b3cc69
= <andrewboudreau@gmail.com>
2026-07-03 11:24:43 -0500

    Match func_ov073_0212081c

---
+++ src/func_ov073_0212081c.c
@@ -0,0 +1,8 @@
+int func_ov073_0212081c(char* self)
+{
+    *(unsigned short*)(self + 0x100) = 0xa;
+    *(int*)(self + 0x4b4) = 0;
+    *(int*)(self + 0x4b8) = 0;
+    *(unsigned char*)(self + 0x4c5) = 0xff;
+    return 1;
+}
