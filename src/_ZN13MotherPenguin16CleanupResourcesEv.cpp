e0f2825c191eafa2fe9ec26c69ea53409d89383d
= <andrewboudreau@gmail.com>
2026-08-26 04:47:11 -0500

    Migrate MotherPenguin to a real C++ class

--- src/_ZN13MotherPenguin16CleanupResourcesEv.cpp
+++ src/_ZN13MotherPenguin16CleanupResourcesEv.cpp
@@ -1,16 +1,20 @@
 //cpp
-#include "types.h"
+// @symbol _ZN13MotherPenguin16CleanupResourcesEv
+#include "MotherPenguin.h"
+#include "SharedFilePtr.h"
+
 extern "C" {
-struct SharedFilePtr { u32 data[4]; };
-void _ZN13SharedFilePtr7ReleaseEv(struct SharedFilePtr *self);
-extern struct SharedFilePtr data_ov018_02113c00;
-extern struct SharedFilePtr* data_ov018_02112c0c[2];
-extern struct SharedFilePtr* data_ov018_02112c04[2];
-int _ZN13MotherPenguin16CleanupResourcesEv(void){
-  int i;
-  _ZN13SharedFilePtr7ReleaseEv(&data_ov018_02113c00);
-  for(i=0;i<2;i++) _ZN13SharedFilePtr7ReleaseEv(data_ov018_02112c0c[i]);
-  for(i=0;i<2;i++) _ZN13SharedFilePtr7ReleaseEv(data_ov018_02112c04[i]);
-  return 1;
+extern SharedFilePtr data_ov018_02113c00;
+extern SharedFilePtr *data_ov018_02112c0c[2];
+extern SharedFilePtr *data_ov018_02112c04[2];
 }
+
+int MotherPenguin::CleanupResources()
+{
+    data_ov018_02113c00.Release();
+    for (int i = 0; i < 2; i++)
+        data_ov018_02112c0c[i]->Release();
+    for (int i = 0; i < 2; i++)
+        data_ov018_02112c04[i]->Release();
+    return 1;
 }
--- src/_ZN13MotherPenguin8BehaviorEv.cpp
+++ src/_ZN13MotherPenguin8BehaviorEv.cpp
@@ -3,22 +3,18 @@
 /* recovered: named members + shared header, real C++ method */
 #include "MotherPenguin.h"
 extern "C" {
-extern void _ZN9Animation7AdvanceEv(void*);
-extern void _ZN5dCc_c5ClearEv(void*);
-extern void _ZN5dCc_c6UpdateEv(void*);
 extern int func_ov018_02111d28(void*);
 extern void func_ov018_0211235c(void*);
 }
-struct Sub { virtual void m0(); virtual void m1(); virtual void m2(); virtual void m3(); };
 
 int MotherPenguin::Behavior()
 {
-  func_ov018_0211235c((char*)this);
-  _ZN9Animation7AdvanceEv((char*)(Animation *)&mModelAnim);
-  _ZN9Animation7AdvanceEv((char*)&mTextureSequence);
-  _ZN5dCc_c5ClearEv((char*)&mdCcAc_c);
-  _ZN5dCc_c6UpdateEv((char*)&mdCcAc_c);
-  ((Sub*)((char*)&mModelAnim))->m3();
-  func_ov018_02111d28(((char*)this));
-  return 1;
+    func_ov018_0211235c(this);
+    mModelAnim.Animation::Advance();
+    mTextureSequence.Advance();
+    mdCcAc_c.Clear();
+    mdCcAc_c.Update();
+    mModelAnim.UpdateVerts();
+    func_ov018_02111d28(this);
+    return 1;
 }
