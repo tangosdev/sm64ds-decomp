7fbc8fcd77f1f74a9eb56153f03f31d785306af0
= <andrewboudreau@gmail.com>
2026-08-27 16:17:27 -0500

    Use shared ClockPaintingHandShort vtable declaration

--- src/ClockPaintingHandShort_Spawn.cpp
+++ src/ClockPaintingHandShort_Spawn.cpp
@@ -1,12 +1,12 @@
 //cpp
 // @symbol ClockPaintingHandShort_Spawn
 #include "ClockPaintingHandShort.h"
+#include "decl_common.h"
 
 extern "C" {
 void *_ZN7fBase_cnwEj(unsigned int size);
 void _ZN8dActor_cC2Ev(void *self);
 void _ZN5ModelC1Ev(void *self);
-extern void *_ZTV22ClockPaintingHandShort;
 }
 
 extern "C" ClockPaintingHandShort *ClockPaintingHandShort_Spawn()
@@ -15,7 +15,7 @@ extern "C" ClockPaintingHandShort *ClockPaintingHandShort_Spawn()
         (ClockPaintingHandShort *)_ZN7fBase_cnwEj(sizeof(ClockPaintingHandShort));
     if (actor) {
         _ZN8dActor_cC2Ev(actor);
-        *(void **)actor = &_ZTV22ClockPaintingHandShort;
+        *(void **)actor = _ZTV22ClockPaintingHandShort;
         _ZN5ModelC1Ev(&actor->mModel);
     }
     return actor;
