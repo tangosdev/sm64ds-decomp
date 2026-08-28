fb5ac10610e4c775ecb9ce8f3a16e0be728a55a5
= <andrewboudreau@gmail.com>
2026-08-27 21:55:58 -0500

    Use native dBase spawn method

--- src/_ZN7dBase_c5SpawnEjP7fBase_cii.cpp
+++ src/_ZN7dBase_c5SpawnEjP7fBase_cii.cpp
@@ -1,10 +1,9 @@
 //cpp
-/* _ZN7dBase_c5SpawnEjP7fBase_cii @ 0x2013ee8 (arm9) -- tail-call veneer to func_02042ffc (0x2042ffc).
- * ldr ip, [pc]; bx ip; .word 0x2042ffc
- */
-extern "C" {
-extern void func_02042ffc(void);
-void _ZN7dBase_c5SpawnEjP7fBase_cii(void) {
-    func_02042ffc();
-}
+#include "dBase_c.h"
+
+extern "C" fBase_c *func_02042ffc(u32 actorID, fBase_c *parent);
+
+fBase_c *dBase_c::Spawn(u32 actorID, fBase_c *parent, int, int)
+{
+    return func_02042ffc(actorID, parent);
 }
