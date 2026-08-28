bf3536ab3608af48bb5ce0ba01d3367e45c466c2
= <andrewboudreau@gmail.com>
2026-08-27 22:17:01 -0500

    Use native dActor player bump method

--- src/_ZN8dActor_c24BumpedUnderneathByPlayerER6Player.cpp
+++ src/_ZN8dActor_c24BumpedUnderneathByPlayerER6Player.cpp
@@ -1,12 +1,11 @@
 //cpp
-// @symbol _ZN8dActor_c24BumpedUnderneathByPlayerER6Player
-/* recovered: named members + shared header */
-#include "dActor_c.h"
-extern "C" {
-int _ZN8dActor_c24BumpedUnderneathByPlayerER6Player(struct dActor_c *self, char*player) {
-if(*(unsigned char*)(player+0x6de)!=0 && *(int*)(player+0xa8)>0
-   && *(int*)(player+0x60) < self->mPosY)
-  return 1;
-return 0;
-}
+#include "Player.h"
+
+int dActor_c::BumpedUnderneathByPlayer(Player &player)
+{
+    if (player.mIsAirborne != 0 && player.mVertSpeed > 0
+        && player.mPosY < mPosY)
+        return 1;
+
+    return 0;
 }
