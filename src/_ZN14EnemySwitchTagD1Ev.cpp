b824d75b64dbcd23786863b58d7ea2a17755f27e
= <andrewboudreau@gmail.com>
2026-08-29 11:25:20 -0500

    Use typed actor headers for destructors

--- src/_ZN14EnemySwitchTagD1Ev.cpp
+++ src/_ZN14EnemySwitchTagD1Ev.cpp
@@ -1,17 +1,6 @@
 //cpp
 // @symbol _ZN14EnemySwitchTagD1Ev
-
-struct dActor_c {
-    char pad[0xd0];
-    virtual ~dActor_c();
-};
-
-struct dCcAc_c { char pad[0x4]; ~dCcAc_c(); };
-
-struct EnemySwitchTag : dActor_c {
-    dCcAc_c m0;   /* 0xd4 */
-    virtual ~EnemySwitchTag();
-};
+#include "EnemySwitchTag.h"
 
 EnemySwitchTag::~EnemySwitchTag()
 {
