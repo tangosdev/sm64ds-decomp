f60cc0cb60ce2b9df17099dfba30a45ec42ec1d5
= <andrewboudreau@gmail.com>
2026-08-30 00:47:19 -0500

    Promote ActorDerived as one genuine C++ TU

--- src/_ZN7dBase_c5SpawnEjP7fBase_cii.cpp
+++
@@ -1,9 +0,0 @@
-//cpp
-#include "dBase_c.h"
-
-extern "C" fBase_c *func_02042ffc(u32 actorID, fBase_c *parent);
-
-fBase_c *dBase_c::Spawn(u32 actorID, fBase_c *parent, int, int)
-{
-    return func_02042ffc(actorID, parent);
-}
