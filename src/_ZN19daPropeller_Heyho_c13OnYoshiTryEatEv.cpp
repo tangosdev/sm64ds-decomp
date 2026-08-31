a013b5ebdb858de5ff6ab496d5dd6f53a0b0c4a3
andrewboudreau <andrewboudreau@gmail.com>
2026-08-31 11:31:52 -0500

    Promote daPropeller_Heyho_c intact translation unit

--- src/_ZN19daPropeller_Heyho_c13OnYoshiTryEatEv.cpp
+++
@@ -1,7 +0,0 @@
-//cpp
-// @symbol _ZN19daPropeller_Heyho_c13OnYoshiTryEatEv
-#include "daPropeller_Heyho_c.h"
-/* recovered: renamed to Class_Method */
-s32 daPropeller_Heyho_c::OnYoshiTryEat() {
-    return 5;
-}
