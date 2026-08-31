4873aaeed2d2ffd6edf15a7c7d5ce8bef8707789
andrewboudreau <andrewboudreau@gmail.com>
2026-08-31 11:31:52 -0500

    Promote daPropeller_Heyho_c intact translation unit

--- src/_ZN19daPropeller_Heyho_c16OnAimedAtWithEggEv.cpp
+++
@@ -1,9 +0,0 @@
-//cpp
-// @symbol _ZN19daPropeller_Heyho_c16OnAimedAtWithEggEv
-#include "daPropeller_Heyho_c.h"
-// recovered name: FlyGuy_OnAimedAtWithEgg
-/* recovered: renamed to Class_Method */
-/* daPropeller_Heyho_c::OnAimedAtWithEgg - recovered from vtable slot identity */
-s32 daPropeller_Heyho_c::OnAimedAtWithEgg() {
-    return 176128;
-}
