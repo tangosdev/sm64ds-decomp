8a51e011b50b228f8cab2e0ea6e8471a7ecaddc6
andrewboudreau <andrewboudreau@gmail.com>
2026-08-31 09:19:55 -0500

    Reconstruct daPropeller_Heyho_c translation unit

--- include/decl_common.h
+++ include/decl_common.h
@@ -718,7 +718,6 @@ extern int _ZTV6Bullet[];
 extern int _ZTV6Cannon[];
 extern int _ZTV6Coffin[];
 extern int _ZTV6Eyerok[];
-extern int _ZTV6FlyGuy[];
 extern int _ZTV6Klepto[];
 extern int _ZTV6Lakitu[];
 extern int _ZTV6Rabbit[];
