07c77e675be742bee0b102d14ffcc5b07e7b79a3
andrewboudreau <andrewboudreau@gmail.com>
2026-09-01 08:42:41 -0500

    minigames: convert BSC, Card, and MCarlo to real members

--- src/func_ov006_020f8ed8.c
+++ src/func_ov006_020f8ed8.c
@@ -1,6 +1,6 @@
-extern int data_ov006_0213d5ac;
+extern int _ZTV18dMgMCarloCardObj_c;
 void func_ov006_020f8ed8(int *p) {
-    p[0] = (int)&data_ov006_0213d5ac;
+    p[0] = (int)&_ZTV18dMgMCarloCardObj_c;
     p[1] = 0;
     p[2] = 0;
 }
