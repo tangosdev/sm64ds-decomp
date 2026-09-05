5c02948bf077a9e0e80ff79bfffa886df0853a9c
andrewboudreau <andrewboudreau@gmail.com>
2026-09-04 23:37:07 -0500

    Restore legacy NewSimple implementation before rename

--- src/_ZN8Particle6System9NewSimpleEjiii.cpp
+++ src/_ZN8Particle6System9NewSimpleEjiii.cpp
@@ -1,10 +1,12 @@
-//cpp
-// @symbol _ZN8Particle6System9NewSimpleEjiii
-#include "Particle__System.h"
+// @symbol _ZN8Particle6System9NewSimpleEj5Fix12IiES2_S2_
+// Particle::System::NewSimple - creates a new particle system at position
+#include "types.h"
+#include "Particle.h"
 
-void Particle::System::NewSimple(u32 definitionID, Fix12i x,
-                                 Fix12i y, Fix12i z)
-{
+extern struct Particle* data_0209ee74;
+extern void* _ZN8Particle7Manager9AddSystemEiR7Vector3(void* mgr, u32 uniqueID, Vector3* pos);
+
+void* _ZN8Particle6System9NewSimpleEj5Fix12IiES2_S2_(u32 uniqueID, s32 x, s32 y, s32 z) {
     Vector3 pos;
     s32 xv = x >> 3;
     s32 yv = y >> 3;
@@ -12,5 +14,6 @@ void Particle::System::NewSimple(u32 definitionID, Fix12i x,
     pos.x = xv;
     pos.y = yv;
     pos.z = zv;
-    data_0209ee74->mManager->AddSystem(definitionID, pos);
+    void* mgr = data_0209ee74->mManager;
+    return _ZN8Particle7Manager9AddSystemEiR7Vector3(mgr, uniqueID, &pos);
 }
