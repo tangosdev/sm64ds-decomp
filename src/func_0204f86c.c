2cc37f12be591476fa8e21cce979a05993b5ad2c
TITLETEXRV <bmanus2@gmail.com>
2026-09-09 08:35:00 -0400

    Seven sound-command wrappers forward the arguments riding in r1 and r2

--- src/func_0204f86c.c
+++ src/func_0204f86c.c
@@ -1,2 +1,2 @@
-extern void func_0205aaf4(int);
-void func_0204f86c(char **c) { unsigned char *p = (unsigned char *)*c; if (p) func_0205aaf4(p[0x3c]); }
+extern void func_0205aaf4(int, int, int);
+void func_0204f86c(char **c, int b, int d) { unsigned char *p = (unsigned char *)*c; if (p) func_0205aaf4(p[0x3c], b, d); }
