413c3a0f54ac5dee8bc474edf760d993aa07e709
andrewboudreau <andrewboudreau@gmail.com>
2026-09-10 19:46:25 -0500

    Use native construction for the falling-block factory

--- include/daObjFl_Fall_Block_c.h
+++ include/daObjFl_Fall_Block_c.h
@@ -4,20 +4,23 @@
 #include "types.h"
 #include "daObjFallBlock_c.h"
 
-/**
- * Lethal Lava Land falling block.
- *
- * A daObjFallBlock_c leaf: no fields of its own. InitResources and
- * CleanupResources load and free this stage's files.
- */
+/* Lethal Lava Land's falling block. */
 
 #ifdef __cplusplus
 
+extern "C" void *_ZN7fBase_cnwEj(unsigned size);
+
 struct daObjFl_Fall_Block_c : daObjFallBlock_c {
+    static void *operator new(unsigned long size);
     virtual ~daObjFl_Fall_Block_c() {}
     int CleanupResources();
     int InitResources();
 };
+
+inline void *daObjFl_Fall_Block_c::operator new(unsigned long size)
+{
+    return _ZN7fBase_cnwEj(size);
+}
 
 typedef char daObjFl_Fall_Block_c_size_must_be_0x34c[sizeof(daObjFl_Fall_Block_c) == 0x34c ? 1 : -1];
 
