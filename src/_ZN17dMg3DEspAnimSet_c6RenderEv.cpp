b4257ac0ff62de1a1bb712dc4644a5a1c0990e34
andrewboudreau <andrewboudreau@gmail.com>
2026-09-22 08:08:22 -0500

    Use native animation-set members and indexed loops

--- src/_ZN17dMg3DEspAnimSet_c6RenderEv.cpp
+++ src/_ZN17dMg3DEspAnimSet_c6RenderEv.cpp
@@ -1,41 +1,21 @@
 //cpp
 // @symbol _ZN17dMg3DEspAnimSet_c6RenderEv
-/* recovered: shared common types */
 #include "common.h"
 #include "dMg3DEspAnimSet_c.h"
-extern "C" {
 
+extern "C" {
 extern Matrix4x3 data_020a0e68;
-void Matrix4x3_FromTranslation(void* m, int x, int y, int z);
+void Matrix4x3_FromTranslation(Matrix4x3* matrix, int x, int y, int z);
 }
-struct ModelComponents;
-struct Obj {
-    void* vt;
-    virtual void v0(); virtual void v1(); virtual void v2();
-    virtual void v3(); virtual void v4();
-    virtual void m(void* arg);
-};
-#pragma opt_strength_reduction off
+
 void dMg3DEspAnimSet_c::Render()
 {
-    char *c = (char *)this;
-    int i;
-    char *r5;
-    char *r4;
-    void *zero;
     Matrix4x3_FromTranslation(&data_020a0e68, 0x8c000, 0x80000, 0x40000);
-    r5 = c;
-    r4 = c + 0x12c;
-    i = 0;
-    zero = (void*)i;
-    do {
-        if (*(int*)(c + i*4 + 0x168) != 0) {
-            *(Matrix4x3*)(r5 + 0x1c) = data_020a0e68;
-            ((MaterialChanger*)r4)->Update(*(ModelComponents*)(r5 + 8));
-            ((Obj*)r5)->m(zero);
+    for (int i = 0; i < 3; i++) {
+        if (unk_168[i] != 0) {
+            mModels[i].mat4x3 = data_020a0e68;
+            mMaterialChangers[i].Update(mModels[i].data);
+            mModels[i].Render(0);
         }
-        i++;
-        r5 += 0x64;
-        r4 += 0x14;
-    } while (i < 3);
+    }
 }
--- src/_ZN17dMg3DEspAnimSet_c8BehaviorEv.cpp
+++ src/_ZN17dMg3DEspAnimSet_c8BehaviorEv.cpp
@@ -2,49 +2,32 @@
 // @symbol _ZN17dMg3DEspAnimSet_c8BehaviorEv
 #include "dMg3DEspAnimSet_c.h"
 
-extern "C" {
-int _Z15ApproachLinear2Rsss(short* p, short a, short b);
-void _ZN9Animation7AdvanceEv(void* a);
-int _ZN9Animation8FinishedEv(void* a);
-}
+int ApproachLinear2(short& value, short target, short step);
 
 void dMg3DEspAnimSet_c::Behavior()
 {
-    char *self = (char *)this;
-    int sb = 0;
-    if (*(short*)(self + 0x178) != 0) {
-        if (_Z15ApproachLinear2Rsss((short*)(self + 0x17a), 0, 1)) {
-            *(short*)(self + 0x17a) = *(short*)(self + 0x17c);
-            sb = 1;
-            if (*(short*)(self + 0x178) > 0) {
-                *(short*)(int)(self + 0x178) -= 1;
+    int activationPending = 0;
+    if (unk_178 != 0) {
+        if (ApproachLinear2(unk_17a, 0, 1)) {
+            unk_17a = unk_17c;
+            activationPending = 1;
+            if (unk_178 > 0) {
+                unk_178 -= 1;
             }
         }
     }
-    {
-        int zero = 0;
-        int i;
-        char* a = self;
-        char* b = self + 0x12c;
-        char* cc = self;
-        for (i = 0; i < 3; i++) {
-            if (((int*)(self + 0x168))[i] != 0) {
-                _ZN9Animation7AdvanceEv(a + 0x50);
-                _ZN9Animation7AdvanceEv(b);
-                if (_ZN9Animation8FinishedEv(a + 0x50)) {
-                    ((int*)(self + 0x168))[i] = 0;
-                }
-            } else {
-                if (sb == 1) {
-                    ((int*)(self + 0x168))[i] = 1;
-                    *(int*)(a + 0x58) = zero;
-                    sb = zero;
-                    *(int*)(cc + 0x134) = zero;
-                }
+    for (int i = 0; i < 3; i++) {
+        if (unk_168[i] != 0) {
+            mModels[i].Advance();
+            mMaterialChangers[i].Advance();
+            if (mModels[i].Finished()) {
+                unk_168[i] = 0;
             }
-            a += 0x64;
-            b += 0x14;
-            cc += 0x14;
+        } else if (activationPending == 1) {
+            unk_168[i] = 1;
+            mModels[i].currFrame = 0;
+            activationPending = 0;
+            mMaterialChangers[i].currFrame = 0;
         }
     }
 }
